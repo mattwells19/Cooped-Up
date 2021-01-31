6762755ebdd7abfe62e9b8dc340439bcfcd011f6
mattwells19 <mwells21@uncc.edu>
2021-01-31 16:38:33 -0500

    Add socket io and restructure

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const http = require("http").createServer();
+const io = require("socket.io")(http, { cors: { origin: "http://localhost:8080" } });
+
+io.on("connection", (socket) => {
+  console.log("connected");
+
+  socket.on("message", (data) => {
+    io.emit("new_message", data);
+  });
+});
+
+http.listen(4000, () => console.log("Listening on port 4000"))
