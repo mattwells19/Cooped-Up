12ee0cbde0f0c3d586069042b367d312a67e5be4
mattwells19 <mwells21@uncc.edu>
2021-02-23 20:39:25 -0500

    Fix linting error

--- web/src/components/Actions.tsx
+++ web/src/components/Actions.tsx
@@ -1,6 +1,5 @@
 import { Button, Wrap, WrapItem, Text, VStack, ButtonProps, useColorModeValue } from "@chakra-ui/react";
 import * as React from "react";
-import { useGameState } from "../contexts/GameStateContext/GameStateContext";
 
 const WrappedButton: React.FC<Omit<ButtonProps, "width">> = ({ children, ...props }) => (
   <WrapItem>
@@ -8,36 +7,33 @@ const WrappedButton: React.FC<Omit<ButtonProps, "width">> = ({ children, ...prop
   </WrapItem>
 );
 
-const Actions: React.FC = () => {
-  const gameState = useGameState();
-  return (
-    <VStack>
-      <Text fontSize="lg" alignSelf="flex-start">Actions</Text>
-      <Wrap
-        align="center"
-        backgroundColor={useColorModeValue("gray.200", "gray.700")}
-        justify="space-evenly"
-        borderRadius="10px"
-        height="280px"
-        padding="3"
-        width="324px"
-        sx={{
-          ul: {
-            height: "100%",
-          },
-        }}
-      >
-        <WrappedButton colorScheme="purple">Collect Tax</WrappedButton>
-        <WrappedButton colorScheme="blue">Steal</WrappedButton>
-        <WrappedButton colorScheme="gray">Assassinate</WrappedButton>
-        <WrappedButton variant="outline">Foreign Aid</WrappedButton>
-        <WrappedButton colorScheme="yellow">Inquisite</WrappedButton>
-        <WrappedButton variant="outline">Income</WrappedButton>
-        <WrappedButton colorScheme="green">Exchange</WrappedButton>
-        <WrappedButton colorScheme="red">Coup</WrappedButton>
-      </Wrap>
-    </VStack>
-  );
-};
+const Actions: React.FC = () => (
+  <VStack>
+    <Text fontSize="lg" alignSelf="flex-start">Actions</Text>
+    <Wrap
+      align="center"
+      backgroundColor={useColorModeValue("gray.200", "gray.700")}
+      justify="space-evenly"
+      borderRadius="10px"
+      height="280px"
+      padding="3"
+      width="324px"
+      sx={{
+        ul: {
+          height: "100%",
+        },
+      }}
+    >
+      <WrappedButton colorScheme="purple">Collect Tax</WrappedButton>
+      <WrappedButton colorScheme="blue">Steal</WrappedButton>
+      <WrappedButton colorScheme="gray">Assassinate</WrappedButton>
+      <WrappedButton variant="outline">Foreign Aid</WrappedButton>
+      <WrappedButton colorScheme="yellow">Inquisite</WrappedButton>
+      <WrappedButton variant="outline">Income</WrappedButton>
+      <WrappedButton colorScheme="green">Exchange</WrappedButton>
+      <WrappedButton colorScheme="red">Coup</WrappedButton>
+    </Wrap>
+  </VStack>
+);
 
 export default Actions;
